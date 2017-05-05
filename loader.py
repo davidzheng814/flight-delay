a6637eff2df6844989050278bfc8af366084568c
David Zheng <dzd123@mit.edu>
2017-05-04 20:16:33 -0400

    Add ability to load feature vectors as well as time till next large delay.

--- loader.py
+++ loader.py
@@ -2,55 +2,124 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 import datetime
+import math
 
-def is_valid_flight(x, start_window):
-    if (len(str(x['ORIGIN_AIRPORT'])) != 3 or
-        len(str(x['DESTINATION_AIRPORT'])) != 3):
-        return False
-
-    arr_time = x['SCHEDULED_ARRIVAL']
-    arr_dt = datetime.datetime(x['YEAR'], x['MONTH'], x['DAY'], arr_time/100, arr_time%100)
-    duration = datetime.timedelta(minutes=x['SCHEDULED_TIME'])
-    dep_dt = arr_dt - duration
-
-    end_window = start_window + datetime.timedelta(hours=1)
-    if arr_dt < start_window or dep_dt > end_window:
-        return False
-
-    return True
+#TODO FIX TIME ZONES
+# TODO consider edge case of no delay/long period of no delay
 
 def load_data(nrows=30000):
-    return pd.read_csv('flights.csv', nrows=nrows)
+    flights = pd.read_csv('flights.csv', nrows=nrows)
+    flights = flights[flights['DEPARTURE_DELAY'].notnull()]
+    return flights
+
+def row_to_dt(x, depart=True):
+    col = 'SCHEDULED_DEPARTURE' if depart else 'SCHEDULED_ARRIVAL'
+    return datetime.datetime(x['YEAR'], x['MONTH'], x['DAY'], x[col]/100, x[col]%100)
+
+def get_delay_times(flights, year, month, day, hour, min_delay=60):
+    def is_valid_flight(x, start_dt):
+        if (len(str(x['ORIGIN_AIRPORT'])) != 3 or
+            len(str(x['DESTINATION_AIRPORT'])) != 3):
+            return False
+
+        dep_dt = row_to_dt(x, depart=True)
+        if dep_dt > start_dt and x['DEPARTURE_DELAY'] >= min_delay:
+            return True
+
+        return False
+
+    # consider delays after start_dt.
+    start_dt = datetime.datetime(year, month, day, hour) + datetime.timedelta(hours=1)
+
+    flights = flights[flights.apply(lambda x: is_valid_flight(x, start_dt), axis=1)]
+    flights['TIMES'] = flights.apply(lambda x: (row_to_dt(x) - start_dt).seconds/60 + 1, axis=1)
+    flights = flights[['ORIGIN_AIRPORT', 'TIMES']]
+    flights = flights.groupby('ORIGIN_AIRPORT').min().reset_index()
+
+    return {x['ORIGIN_AIRPORT']:x['TIMES'] for i, x in flights.iterrows()}
 
 def load_graph(flights, year, month, day, hour):
+    def is_valid_flight(x, start_window):
+        if (len(str(x['ORIGIN_AIRPORT'])) != 3 or
+            len(str(x['DESTINATION_AIRPORT'])) != 3):
+            return False
+
+        arr_dt = row_to_dt(x, depart=False)
+        duration = datetime.timedelta(minutes=x['SCHEDULED_TIME'])
+        dep_dt = arr_dt - duration
+
+        end_window = start_window + datetime.timedelta(hours=1)
+        if arr_dt < start_window or dep_dt > end_window:
+            return False
+
+        return True
+
     start_window = datetime.datetime(year, month, day, hour)
     flights = flights[flights.apply(lambda x:is_valid_flight(x, start_window), axis=1)]
 
     flights = flights[['ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'DEPARTURE_DELAY']]
-    flights = flights[flights['DEPARTURE_DELAY'].notnull()]
     delay_avgs = (flights
                   .groupby(['ORIGIN_AIRPORT', 'DESTINATION_AIRPORT'])
-                  .agg(['mean', 'count'])
+                  .agg(['mean', 'count', 'sum', 'std'])
                   .reset_index())
     delay_avgs.columns = [' '.join(col).strip() for col in delay_avgs.columns.values]
     delay_avgs = delay_avgs.rename(
-        columns={'DEPARTURE_DELAY mean': 'MEAN_DELAY', 'DEPARTURE_DELAY count': 'COUNT'})
+        columns={'DEPARTURE_DELAY mean': 'MEAN', 
+                 'DEPARTURE_DELAY count': 'COUNT',
+                 'DEPARTURE_DELAY sum': 'SUM',
+                 'DEPARTURE_DELAY std': 'STD'})
 
     G = nx.from_pandas_dataframe(
-        delay_avgs, 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', edge_attr=['MEAN_DELAY', 'COUNT'],
+        delay_avgs, 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', edge_attr=['MEAN', 'COUNT', 'SUM', 'STD'],
         create_using=nx.DiGraph())
 
     return G
 
-def get_stats(G, node):
-    in_airports = G.in_degree(node)
-    out_airports = G.out_degree(node)
-    in_flights = G.in_degree(node, weight='COUNT')
-    out_flights = G.out_degree(node, weight='COUNT')
+def get_feature_vectors(G, year, month, day, hour):
+    time = datetime.datetime(year, month, day, hour)
 
-    betweenness = nx.betweenness_centrality(G)[node]
+    features = {}
+    month_vec = np.zeros(12)
+    month_vec[month - 1] = 1
+    day_vec = np.zeros(7)
+    day_vec[time.weekday()] = 1
+    hour_vec = np.zeros(24)
+    hour_vec[hour] = 1
+    time_vec = np.concatenate((month_vec, day_vec, hour_vec))
 
-    return in_airports, out_airports, in_flights, out_flights, betweenness
+    for node in G.nodes():
+        in_flights = G.in_degree(node, weight='COUNT')
+        out_flights = G.out_degree(node, weight='COUNT')
+        mean_in_delay = G.in_degree(node, weight='MEAN')
+        mean_out_delay = G.out_degree(node, weight='MEAN')
+
+        std_in_delay = G.in_degree(node, weight='STD')
+        if math.isnan(std_in_delay):
+            std_in_delay = 0
+        std_out_delay = G.out_degree(node, weight='STD')
+        if math.isnan(std_out_delay):
+            std_out_delay = 0
+
+        tot_in_delay = G.in_degree(node, weight='SUM')
+        tot_out_delay = G.out_degree(node, weight='SUM')
+
+        features[node] = [in_flights, out_flights, mean_in_delay, mean_out_delay,
+            std_in_delay, std_out_delay, tot_in_delay, tot_out_delay]
+
+    betweenness = nx.betweenness_centrality(G)
+    # katz = nx.katz_centrality(G, alpha=0.01, max_iter=1000)
+    # weighted_katz = nx.katz_centrality(G, weight='SUM')
+    clustering = nx.clustering(nx.Graph(G))
+    hubs, auth = nx.hits(G)
+
+    for node in G.nodes():
+        features[node].extend([betweenness[node], 
+            # katz[node], weighted_katz[node], 
+            clustering[node], hubs[node], auth[node]])
+        arr = np.array(features[node])
+        features[node] = np.concatenate((arr, time_vec))
+
+    return features
 
 def get_correlation(G): 
     adj = nx.adjacency_matrix(G) 
@@ -65,10 +134,3 @@ def get_correlation(G):
 
     return cov_matrix
 
-flights = load_data()
-G = load_graph(flights, 2015, 1, 1, 5)
-
-# print get_stats(G, 'LAX') 
-
-# test get_correlation
-print get_correlation(G) 
