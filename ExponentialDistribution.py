f576d6074e078ccebe65b6093cf76c4bcdd221fc
Ajay Saini <ajays235@mit.edu>
2017-05-13 00:09:06 -0400

    cleaned up code

--- ExponentialDistribution.py
+++ ExponentialDistribution.py
@@ -18,7 +18,7 @@ class ExponentialDistribution():
                 at each iteration of training
         Note: converged with learning_rate=.0000001
     '''
-    def fit(self, X, T, regularization=1, learning_rate=.0000001, early_stopping=True, max_iter=35000, tol=.00001):
+    def fit(self, X, T, regularization=1, learning_rate=.0000001, early_stopping=True, max_iter=35000, tol=.1):
         if len(X) == 0:
             return
         X = np.array(X)
@@ -67,33 +67,8 @@ class ExponentialDistribution():
 
     # returns a d by 1 vector
     def gradient(self, X, T, regularization=0):
-        # print np.expand_dims(np.exp(X.dot(self.betas.transpose())), axis=1).shape
-        #print T.shape
-        # if T.ndim > 1:
-        #     T = np.squeeze(T, axis=1)
-        # print "X:",X.shape
-        # print "T:",T.shape
-        # print "first term:",np.sum(X, axis=0).shape
-        # print np.multiply(np.expand_dims(np.exp(X.dot(self.betas.transpose())), axis=1), T).shape
-        # print X.transpose().dot(np.multiply(np.expand_dims(np.exp(X.dot(self.betas.transpose())), axis=1), T)).shape
-        # print "reg shape:",(2 * regularization * np.absolute(self.betas).transpose()).shape
-        # a = np.sum(X, axis=0)
-        # #b = X.transpose().dot(np.multiply(np.expand_dims(np.exp(X.dot(self.betas.transpose())), axis=1), T))
-        # print "exp shape:",np.exp(X.dot(self.betas.transpose())).shape
-        # print "prod shape:",(np.exp(X.dot(self.betas.transpose())) * T).shape
-        # print "const:",np.multiply(np.exp(X.dot(self.betas.transpose())), np.squeeze(T, axis=1)).shape
-        # b = X.transpose().dot(np.multiply(np.exp(X.dot(self.betas.transpose())), np.squeeze(T, axis=1)))
-        # print "b shape:",b.shape
-        # print "a:",a
-        # print "b: ",b
-        # print "a - b:",(a - b)
-        #return np.sum(X, axis=0) - X.transpose().dot(np.multiply(np.expand_dims(np.exp(X.dot(self.betas.transpose())), axis=1), T)).sum(axis=0) - 2 * regularization * np.absolute(self.betas).transpose()
         return np.sum(X, axis=0) - X.transpose().dot(np.multiply(np.exp(X.dot(self.betas.transpose())), T)) - 2 * regularization * np.absolute(self.betas).transpose()
-        #print "Return shape:",(np.sum(X, axis=0) - X.transpose().dot(np.multiply(np.expand_dims(np.exp(X.dot(self.betas.transpose())), axis=1), T))).shape
-        #return np.sum(X, axis=0).expand_dims(axis=1) - X.transpose().dot(np.multiply(np.exp(X.dot(self.betas.transpose())), T))
-
-        #sys.exit(0)
-
+       
     # returns an n by 1 vector
     def pdf(self, X, T):
         X = np.array(X)
@@ -137,42 +112,41 @@ class ExponentialDistribution():
 
         plt.savefig("./ROCCurvesForDelayPredictionBetween{}and{}HoursStandardizedData_Reg10".format(num_hours_start, num_hours_end), format="png")
 
-# def TestExponentialEstimation():
-#     n = 100
-#     d = 15
-#     fake_X = np.random.randint(0, 2, size=(n, d))
-#     fake_T = np.random.randint(1, 10, size=(n, 1))
+def TestExponentialEstimation():
+    n = 100
+    d = 15
+    fake_X = np.random.randint(0, 2, size=(n, d))
+    fake_T = np.random.randint(1, 10, size=(n, 1))
 
-#     dist = ExponentialDistribution()
+    dist = ExponentialDistribution()
 
-#     # set early_stopping to True to stop training at optimal likelihood
-#     betas, likelihood_vals, gradient_norm_vals = dist.fit(fake_X, fake_T, regularization=1, early_stopping=False, max_iter=10000)
-#     iters = range(len(likelihood_vals))
+    # set early_stopping to True to stop training at optimal likelihood
+    betas, likelihood_vals, gradient_norm_vals = dist.fit(fake_X, fake_T, regularization=1, early_stopping=False, max_iter=10000)
+    iters = range(len(likelihood_vals))
 
-#     #print "PDF:",dist.pdf(fake_X, fake_T)
-#     #print "CDF:",dist.cdf(fake_X, fake_T)
-#     print "Final Betas:",betas
-#     print "Final Likelihood:",likelihood_vals[-1]
-#     print "Final Gradient Norm:",gradient_norm_vals[-1]
+    #print "PDF:",dist.pdf(fake_X, fake_T)
+    #print "CDF:",dist.cdf(fake_X, fake_T)
+    print "Final Betas:",betas
+    print "Final Likelihood:",likelihood_vals[-1]
+    print "Final Gradient Norm:",gradient_norm_vals[-1]
 
-#     #dist.makeROCCurvesForTestData(fake_X, fake_T)
-#     plt.figure(1)
-#     plt.subplot(211)
-#     plt.plot(iters, likelihood_vals, label="Log Likelihood")
-#     #plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
-#     plt.title("Log Likelihood for Estimation with No Regularization")
-#     plt.xlabel("Iteration Number")
+    #dist.makeROCCurvesForTestData(fake_X, fake_T)
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(iters, likelihood_vals, label="Log Likelihood")
+    #plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
+    plt.title("Log Likelihood for Estimation with No Regularization")
+    plt.xlabel("Iteration Number")
     
-#     plt.subplot(212)
-#     plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
-#     plt.xlabel("Iteration Number")
-#     #plt.ylabel("Value")
-#     plt.title("Gradient Norm for Estimation with No Regularization")
-#     #plt.legend(loc="upper right")
-#     plt.show()
+    plt.subplot(212)
+    plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
+    plt.xlabel("Iteration Number")
+    #plt.ylabel("Value")
+    plt.title("Gradient Norm for Estimation with No Regularization")
+    #plt.legend(loc="upper right")
+    plt.show()
 
 
-<<<<<<< Updated upstream
 def TestEstimationOnTrainingData():
     X, T = loadTrainingData()
     X = (X - np.mean(X, axis=0)) / (np.std(X, axis=0) + 1e-11)
@@ -181,65 +155,65 @@ def TestEstimationOnTrainingData():
     X_train, X_test, T_train, T_test = train_test_split(X, T, test_size=.2)
     dist = ExponentialDistribution()
 
-    # betas, likelihood_vals, gradient_norm_vals = dist.fit(X_train, T_train, learning_rate=1e-6, regularization=10, early_stopping=False, max_iter=1000000)
-    # iters = range(len(likelihood_vals))
-    # print "Final betas:",betas
-    # print "Final Likelihood:",likelihood_vals[-1]
-    # print "Final Gradient Norm:",gradient_norm_vals[-1]
-    # f = open("FinalBetasStandardizedDataReg10.json", "w")
-    # json.dump(betas.tolist(), f)
-    # f.close()
+    betas, likelihood_vals, gradient_norm_vals = dist.fit(X_train, T_train, learning_rate=1e-6, regularization=1, early_stopping=False, max_iter=1000000)
+    iters = range(len(likelihood_vals))
+    print "Final betas:",betas
+    print "Final Likelihood:",likelihood_vals[-1]
+    print "Final Gradient Norm:",gradient_norm_vals[-1]
+    f = open("FinalBetasReg1_BOS.json", "w")
+    json.dump(betas.tolist(), f)
+    f.close()
 
-    # f = open("FinalLikelihoodAndGradStandardizedDataReg10.json", "w")
-    # json.dump([likelihood_vals, gradient_norm_vals], f)
-    # f.close()
+    f = open("FinalLikelihoodAndGradReg1_BOS.json", "w")
+    json.dump([likelihood_vals, gradient_norm_vals], f)
+    f.close()
 
-    # f = open("FinalBetasStandardizedData.json", "r")
-    # betas = json.load(f)
-    # f.close()
-    # dist.setBetas(betas)
-    # dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=4, num_hours_end=5)
-    # dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=6, num_hours_end=9)
-    # dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=10, num_hours_end=13)
-    # dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=14, num_hours_end=17)
-    # dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=17, num_hours_end=20)
-    # dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=21, num_hours_end=24)
+    f = open("FinalBetasReg1_BOS.json", "r")
+    betas = json.load(f)
+    f.close()
+    dist.setBetas(betas)
+    dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=4, num_hours_end=5)
+    dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=6, num_hours_end=9)
+    dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=10, num_hours_end=13)
+    dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=14, num_hours_end=17)
+    dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=17, num_hours_end=20)
+    dist.makeROCCurvesForTestData(X_test, T_test, num_hours_start=21, num_hours_end=24)
 
-    # f = open("./FinalLikelihoodAndGradStandardizedDataReg10.json", "r")
-    # vals = json.load(f)
-    # iters = range(len(vals[0]))
-    # f.close()
-    # likelihood_vals = vals[0]
-    # gradient_norm_vals = vals[1]
+    f = open("./FinalLikelihoodAndGradStandardizedDataReg10.json", "r")
+    vals = json.load(f)
+    iters = range(len(vals[0]))
+    f.close()
+    likelihood_vals = vals[0]
+    gradient_norm_vals = vals[1]
 
 
-    # plt.figure(1)
-    # # # plt.subplot(211)
-    # # plt.plot(iters, likelihood_vals, label="Log Likelihood")
-    # # #plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
-    # # plt.title("Log Likelihood for Estimation with Regularization = 10")
-    # # plt.xlabel("Iteration Number")
-    # # plt.show()
-    # # sys.exit(0)
-    
-    # # #plt.subplot(212)
-    # plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
-    # plt.xlabel("Iteration Number")
-    # #plt.ylabel("Value")
-    # plt.title("Gradient Norm for Estimation with Regularization = 10")
-    # #plt.legend(loc="upper right")
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(iters, likelihood_vals, label="Log Likelihood")
+    #plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
+    plt.title("Log Likelihood for Estimation with Regularization = 10")
+    plt.xlabel("Iteration Number")
     # plt.show()
+    # sys.exit(0)
+    
+    plt.subplot(212)
+    plt.plot(iters, gradient_norm_vals, label="Gradient Norm")
+    plt.xlabel("Iteration Number")
+    #plt.ylabel("Value")
+    plt.title("Gradient Norm for Estimation with Regularization = 10")
+    #plt.legend(loc="upper right")
+    plt.show()
 
 
 def loadTrainingData():
-    f = open("./training_data/X.txt", "r")
+    f = open("./training_data/X_BOS.txt", "r")
     X = []
     T = []
     for line in f:
         l = [float(x) for x in line.split(',')]
         X.append(l)
     f.close()
-    f = open("./training_data/T.txt", "r")
+    f = open("./training_data/T_BOS.txt", "r")
     for line in f:
         T.append(float(line))
     f.close()
@@ -248,6 +222,8 @@ def loadTrainingData():
     print len(T)
     return X, T
 
-if __name__ == '__main__':
-  TestExponentialEstimation()
+TestEstimationOnTrainingData()
+
+# if __name__ == '__main__':
+#   TestExponentialEstimation()
 
